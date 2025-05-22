07f070d9ef48546e0cf0bc1d1646c06017da2644
Dominik Rapp <rapp.dominik@hotmail.com>
2025-05-22 16:43:44 +0200

    Design changed – first stage of responsive layout

--- assets/scripts/templates.js
+++ assets/scripts/templates.js
@@ -2,8 +2,8 @@ function getPokemonCardTemplate(pokemonData, backgroundColor, typeIconsHTML) {
     return `
         <div class="pokemon-card">
             <div class="pokemon-card-header">
-                <span class="pokemon-id">#${pokemonData.id}</span>
-                <span class="pokemon-name">${capitalizeFirstLetter(pokemonData.name)}</span>
+                <p class="pokemon-id">Id: ${pokemonData.id}</p>
+                <span class="pokemon-name collor-black">${capitalizeFirstLetter(pokemonData.name)}</span>
             </div>
             <div class="pokemon-card-image" style="background-color: ${backgroundColor};">
                 <img src="${pokemonData.sprites.other['official-artwork'].front_default}" alt="${pokemonData.name}" />
