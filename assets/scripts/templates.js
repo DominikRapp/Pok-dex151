19a1095e9469fbf1c296b6e569f6e02e5f8ae993
Dominik Rapp <rapp.dominik@hotmail.com>
2025-05-22 14:58:00 +0200

    worked on design/css

--- assets/scripts/templates.js
+++ assets/scripts/templates.js
@@ -1,8 +1,16 @@
-function getPokemonCardTemplate(pokeData, backgroundColor, typesContent) {
-    return `<div class="pokemon-card" style="background-color: ${backgroundColor};">
-                <img src="${pokeData.sprites.other['official-artwork'].front_default}" alt="${pokeData.name}" />
-                <h4>${capitalizeFirstLetter(pokeData.name)}</h4>
-                <p>#${pokeData.id}</p>
-                <ul>${typesContent}</ul>
-            </div>`;
+function getPokemonCardTemplate(pokemonData, backgroundColor, typeIconsHTML) {
+    return `
+        <div class="pokemon-card">
+            <div class="pokemon-card-header">
+                <span class="pokemon-id">#${pokemonData.id}</span>
+                <span class="pokemon-name">${capitalizeFirstLetter(pokemonData.name)}</span>
+            </div>
+            <div class="pokemon-card-image" style="background-color: ${backgroundColor};">
+                <img src="${pokemonData.sprites.other['official-artwork'].front_default}" alt="${pokemonData.name}" />
+            </div>
+            <div class="pokemon-card-footer">
+                ${typeIconsHTML}
+            </div>
+        </div>
+    `;
 }
--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const typeIcons = {
+    bug: "assets/img/element-icons/bug.svg",
+    dark: "assets/img/element-icons/dark.svg",
+    dragon: "assets/img/element-icons/dragon.svg",
+    electric: "assets/img/element-icons/electric.svg",
+    fairy: "assets/img/element-icons/fairy.svg",
+    fighting: "assets/img/element-icons/fighting.svg",
+    fire: "assets/img/element-icons/fire.svg",
+    flying: "assets/img/element-icons/flying.svg",
+    ghost: "assets/img/element-icons/ghost.svg",
+    grass: "assets/img/element-icons/grass.svg",
+    ground: "assets/img/element-icons/ground.svg",
+    ice: "assets/img/element-icons/ice.svg",
+    normal: "assets/img/element-icons/normal.svg",
+    poison: "assets/img/element-icons/poison.svg",
+    psychic: "assets/img/element-icons/psychic.svg",
+    rock: "assets/img/element-icons/rock.svg",
+    steel: "assets/img/element-icons/steel.svg",
+    water: "assets/img/element-icons/water.svg"
+};
+
 function renderPokemon(pokeData) {
     const pokedexContainer = document.getElementById('pokedex_container');
     const pokemonCardContent = generatePokemonCard(pokeData);
@@ -7,12 +28,8 @@ function renderPokemon(pokeData) {
 function generatePokemonCard(pokeData) {
     const mainType = pokeData.types[0].type.name;
     const backgroundColor = getTypeColor(mainType);
-    let typesContent = '';
-    for (let typeIndex = 0; typeIndex < pokeData.types.length; typeIndex++) {
-        const typeName = capitalizeFirstLetter(pokeData.types[typeIndex].type.name);
-        typesContent += `<li>${typeName}</li>`;
-    }
-    return getPokemonCardTemplate(pokeData, backgroundColor, typesContent);
+    const typeIconsHTML = getTypeIconsHTML(pokeData.types);
+return getPokemonCardTemplate(pokeData, backgroundColor, typeIconsHTML);
 }
 
 function createTypesList(types, typesContainer) {
@@ -32,25 +49,36 @@ function capitalizeFirstLetter(text) {
 
 function getTypeColor(type) {
     const typeColors = {
-        fire: "#F08030",
-        grass: "#78C850",
-        electric: "#F8D030",
-        water: "#6890F0",
-        ground: "#E0C068",
-        rock: "#B8A038",
-        fairy: "#EE99AC",
-        poison: "#A040A0",
-        bug: "#A8B820",
-        dragon: "#7038F8",
-        psychic: "#FF4FA1",
-        flying: "#A890F0",
-        fighting: "#C03028",
-        normal: "#A8A878",
-        ice: "#98D8D8",
-        ghost: "#705898",
-        dark: "#705848",
-        steel: "#B8B8D0"
+        bug: "#92BC2C",
+        dark: "#595761",
+        dragon: "#0C69C8",
+        electric: "#F2D94E",
+        fire: "#FBA54C",
+        fairy: "#EE90E6",
+        fighting: "#D3425F",
+        flying: "#A1BBEC",
+        ghost: "#5F6DBC",
+        grass: "#5FBD58",
+        ground: "#DA7C4D",
+        ice: "#75D0C1",
+        normal: "#A0A29F",
+        poison: "#B763CF",
+        psychic: "#FA8581",
+        rock: "#C9BB8A",
+        steel: "#5695A3",
+        water: "#539DDF"
     };
-
     return typeColors[type];
+}
+
+function getTypeIconsHTML(types) {
+    return types.map(typeInfo => {
+        const type = typeInfo.type.name.toLowerCase();
+        const iconSrc = typeIcons[type] || "assets/img/element-icons/default.svg"; // Fallback
+        return `
+            <div class="icon ${type}">
+                <img src="${iconSrc}" alt="${type}" title="${capitalizeFirstLetter(type)}" />
+            </div>
+        `;
+    }).join('');
 }
